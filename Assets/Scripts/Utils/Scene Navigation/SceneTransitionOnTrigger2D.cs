d6590aefe4c7fcab1a59bc8f67753c17d19d93cb
Vivek Raman <vr.ac4bf@live.com>
2021-06-13 21:04:26 +0530

    Added level exit and scene navigation

---
+++ Assets/Scripts/Utils/Scene Navigation/SceneTransitionOnTrigger2D.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Utils.SceneNavigation
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class SceneTransitionOnTrigger2D : SceneTransition
+    {
+        private IEnumerator OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.CompareTag("Player")) yield break;
+            yield return new WaitForSeconds(0.3f);
+            MoveToNextScene();
+        }
+    }
+}
